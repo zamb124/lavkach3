705b9f632e040f62bec1576295457aa76b7b0983
viktor-shved <viktor-shved@yandex-team.ru>
2023-06-08 09:59:52 +0300

    Жостка БД

--- core/config.py
+++ core/config.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 class Config(BaseSettings):
     ENV: str = "development"
     DEBUG: bool = True
-    APP_HOST: str = "127.0.0.1"
+    APP_HOST: str = "0.0.0.0"
     APP_PORT: int = 8080
     WRITER_DB_URL: str = f"postgresql+asyncpg://taxi:test@localhost:5432/fastapi"
     READER_DB_URL: str = f"postgresql+asyncpg://taxi:test@localhost:5432/fastapi"
