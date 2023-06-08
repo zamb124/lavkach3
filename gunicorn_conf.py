a4892858f03d5dced3edf1ff96cca52fc6cb2bc4
viktor-shved <viktor-shved@yandex-team.ru>
2023-06-08 10:21:52 +0300

    Жостка БД

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,7 +1,7 @@
 from multiprocessing import cpu_count
 
 # Socket Path
-bind = "127.0.0.1:8000"
+bind = 'unix:/home/viktor-shved/lavkach/gunicorn.sock'
 
 # Worker Options
 workers = cpu_count() + 1
