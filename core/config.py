48edc73b4b1a69bf74207ecd50cb3800c7c25bb5
viktor-shved <viktor-shved@yandex-team.ru>
2023-06-08 10:02:12 +0300

    Жостка БД

--- core/config.py
+++ core/config.py
@@ -8,8 +8,8 @@ logging.basicConfig(level=logging.INFO)
 class Config(BaseSettings):
     ENV: str = "development"
     DEBUG: bool = True
-    APP_HOST: str = "0.0.0.0"
-    APP_PORT: int = 8080
+    APP_HOST: str = "158.160.53.137"
+    APP_PORT: int = 8000
     WRITER_DB_URL: str = f"postgresql+asyncpg://taxi:test@localhost:5432/fastapi"
     READER_DB_URL: str = f"postgresql+asyncpg://taxi:test@localhost:5432/fastapi"
     JWT_SECRET_KEY: str = "fastapi"
