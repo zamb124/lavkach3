6fb31d7151b7951c98fb2daa0b12ac9a815fdb72
viktor-shved <viktor-shved@yandex-team.ru>
2023-11-03 16:46:05 +0400

    востановил тесты, и начинаю чуть их дописывать

--- backend/gunicorn_conf.py
+++ backend/gunicorn_conf.py
@@ -1,13 +1,14 @@
 from multiprocessing import cpu_count
 
 # Socket Path
-bind = 'unix:/fundamental/viktor-shved/lavkach/gunicorn.sock'
+#bind = 'unix:/fundamental/viktor-shved/lavkach/gunicorn.sock'
+bind = "127.0.0.1:8000"
 
 # Worker Options
 workers = cpu_count() + 1
 worker_class = 'uvicorn.workers.UvicornWorker'
 
 # Logging Options
-loglevel = 'info'
+loglevel = 'debug'
 #accesslog = '/fundamental/viktor-shved/lavkach/access_log'
 #errorlog = '/fundamental/viktor-shved/lavkach/error_log'
