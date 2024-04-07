4676b257ebcbc04166466e50b81c21e43409ea9c
viktor-shved <viktor-shved@yandex-team.ru>
2024-04-07 20:27:29 +0400

    BFF: Сделал generic фронт

--- backend/core/config.py
+++ backend/core/config.py
@@ -26,10 +26,11 @@ class Config(BaseSettings):
     DB_USER_TEST: str = os.environ.get("DB_USER_TEST") or 'taxi'
     DB_PASS: str = os.environ.get("DB_PASS") or 'test'
     DB_PASS_TEST: str = os.environ.get("DB_PASS_TEST") or 'test'
+    DB_SSL: str = os.environ.get('DB_SSL') or 'false'
     POSTGRES_TEST_DATABASE_URL: str = f'postgresql+asyncpg://{DB_USER_TEST}:{DB_PASS_TEST}@{DB_HOST_TEST}/postgres'
     TEST_WRITER_DB_URL: str = f'postgresql+asyncpg://{DB_USER_TEST}:{DB_PASS_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}'
-    WRITER_DB_URL: str = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-    READER_DB_URL: str = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    WRITER_DB_URL: str = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?ssl={DB_SSL}'
+    READER_DB_URL: str = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?ssl={DB_SSL}'
     JWT_SECRET_KEY: str = os.environ.get("JWT_SECRET_KEY") or 'secret'
     JWT_ALGORITHM: str = os.environ.get("JWT_ALGORITHM") or 'HS256'
     #SENTRY_SDN: str = os.environ.get("SENTRY_SDN")
