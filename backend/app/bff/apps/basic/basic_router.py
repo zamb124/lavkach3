2cea0061012839f54920f86e652e864094c8dde1
viktor-shved <viktor-shved@yandex-team.ru>
2024-04-17 18:45:50 +0300

    BFF: rels

--- backend/app/bff/apps/basic/basic_router.py
+++ backend/app/bff/apps/basic/basic_router.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 
 from app.bff.apps.basic.company.company import company_router
 from app.bff.apps.basic.product.product import product_router
+from app.bff.apps.basic.product.product_category import product_category_router
+from app.bff.apps.basic.product.product_storage_type import product_storage_type_router
 from app.bff.apps.basic.store.store import store_router
 from app.bff.apps.basic.user.user import user_router
 
@@ -10,6 +12,8 @@ basic_router.include_router(company_router, prefix="/company", tags=["frontend"]
 basic_router.include_router(store_router, prefix="/store", tags=["frontend"])
 basic_router.include_router(user_router, prefix="/user", tags=["frontend"])
 basic_router.include_router(product_router, prefix="/product", tags=["frontend"])
+basic_router.include_router(product_category_router, prefix="/product_category", tags=["frontend"])
+basic_router.include_router(product_storage_type_router, prefix="/product_storage_type", tags=["frontend"])
 
 
 
--- backend/app/bff/apps/basic/product/product_category.py
+++ backend/app/bff/apps/basic/product/product_category.py
@@ -5,11 +5,11 @@ from fastapi.responses import HTMLResponse
 from app.bff.dff_helpers.schema_recognizer import ModelView
 from app.bff.template_spec import templates
 
-product_router = APIRouter()
+product_category_router = APIRouter()
 
 
-@product_router.get("", response_class=HTMLResponse)
-async def product(request: Request):
-    model = ModelView(request, 'basic', 'product')
+@product_category_router.get("", response_class=HTMLResponse)
+async def product_category(request: Request):
+    model = ModelView(request, 'basic', 'product_category')
     return templates.TemplateResponse(request,'widgets/list-full.html', context={'model': model})
 
