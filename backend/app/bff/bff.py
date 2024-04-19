15b612b703406e8b59bc8f6bc9a830aba7c89769
viktor-shved <viktor-shved@yandex-team.ru>
2024-04-19 09:46:11 +0300

    BFF: rels

--- backend/app/bff/bff.py
+++ backend/app/bff/bff.py
@@ -1,18 +1,16 @@
 import uuid
-from typing import Annotated, Optional, Any
+from typing import Annotated, Optional
 
 from fastapi import APIRouter, Depends
 from fastapi import Form
 from fastapi import Request
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel, field_validator, UUID4, model_validator
-from starlette.datastructures import QueryParams
+from pydantic import BaseModel, field_validator, UUID4
 from starlette.responses import Response, JSONResponse
 
-from app.bff.dff_helpers.uttils import clean_filter
 from app.bff.dff_helpers.schema_recognizer import ModelView
+from app.bff.dff_helpers.uttils import clean_filter
 from app.bff.template_spec import templates
-from core.utils.timeit import timed
 
 
 class ExceptionResponseSchema(BaseModel):
@@ -29,12 +27,12 @@ async def root_page(request: Request):
     return templates.TemplateResponse(request, 'index-full.html', context={})
 
 
-@index_router.get("/htmx/footer", response_class=HTMLResponse)
+@index_router.get("/bff/footer", response_class=HTMLResponse)
 async def footer(request: Request):
     return templates.TemplateResponse(request, 'partials/footer.html', context={})
 
 
-@index_router.get("/htmx/topbar", response_class=HTMLResponse)
+@index_router.get("/bff/topbar", response_class=HTMLResponse)
 async def topbar(request: Request):
     return templates.TemplateResponse(request, 'partials/topbar.html', context={})
 
@@ -44,23 +42,23 @@ async def root_page(request: Request):
     return templates.TemplateResponse(request, 'index.html', context={})
 
 
-@index_router.get("/auth/login",responses={"404": {"model": ExceptionResponseSchema}},)
+@index_router.get("/basic/login", responses={"404": {"model": ExceptionResponseSchema}}, )
 async def login(request: Request, response: Response):
-    return templates.TemplateResponse(request, 'auth/login-full.html', context={})
+    return templates.TemplateResponse(request, 'basic/login-full.html', context={})
 
 
 @index_router.post(
-    "/auth/login",
+    "/basic/login",
     responses={"404": {"model": ExceptionResponseSchema}},
 )
 async def login(
         request: Request,
         username: Annotated[str, Form()],
         password: Annotated[str, Form()]):
-
     async with request.scope['env'].basic as a:
         data = await a.login(username, password)
-    return templates.TemplateResponse(request, 'components/write_ls.html', context={'token': data['token'], 'refresh_token': data['refresh_token']})
+    return templates.TemplateResponse(request, 'components/write_ls.html',
+                                      context={'token': data['token'], 'refresh_token': data['refresh_token']})
 
 
 class RefreshTokenSchema(BaseModel):
@@ -74,21 +72,19 @@ async def refresh_token(request: Request, refresh_schema: RefreshTokenSchema):
         return await a.refresh_token(refresh_schema)
 
 
-
 class FilterSchema(BaseModel):
     module: str
     model: str
     prefix: str
 
 
-@index_router.post("/bff/filter", response_class=HTMLResponse)
-async def filter(request: Request, filschema: FilterSchema):
+@index_router.post("/base/filter", response_class=HTMLResponse)
+async def _filter(request: Request, filschema: FilterSchema):
     """
      Универсальный запрос, который отдает фильтр обьекта по его модулю и модели
     """
     htmx_orm = ModelView(request, filschema.module, filschema.model, prefix=filschema.prefix)
     return htmx_orm.get_filter()
-
 
 
 class SearchSchema(BaseModel):
@@ -97,8 +93,8 @@ class SearchSchema(BaseModel):
     query: str = ''
 
 
-@index_router.get("/bff/search", response_class=JSONResponse)
-async def search(request: Request, schema:SearchSchema = Depends(SearchSchema)):
+@index_router.get("/base/search", response_class=JSONResponse)
+async def search(request: Request, schema: SearchSchema = Depends(SearchSchema)):
     """
      Универсальный запрос поиска
     """
@@ -113,11 +109,14 @@ async def search(request: Request, schema:SearchSchema = Depends(SearchSchema)):
         for i in data['data']
     ]
 
+
 class SearchIds(BaseModel):
     module: str
     model: str
     id__in: str
-@index_router.get("/bff/get_by_ids", response_class=JSONResponse)
+
+
+@index_router.get("/base/get_by_ids", response_class=JSONResponse)
 async def get_by_ids(request: Request, schema: SearchSchema = Depends(SearchIds)):
     """
      Универсальный запрос поиска
@@ -142,6 +141,7 @@ class TableSchema(BaseModel):
     cursor: Optional[int] = 0
     prefix: str
 
+
 @index_router.post("/base/table", response_class=HTMLResponse)
 async def table(request: Request, schema: TableSchema):
     """
@@ -157,10 +157,12 @@ async def table(request: Request, schema: TableSchema):
     view = ModelView(request, params=qp, module=schema.module, model=schema.model, prefix=schema.prefix)
     return await view.get_table()
 
+
 class LineSchema(BaseModel):
     module: str
     model: str
     prefix: str
+
 
 @index_router.post("/base/table/line", response_class=HTMLResponse)
 async def line(request: Request, schema: TableSchema):
@@ -174,8 +176,10 @@ async def line(request: Request, schema: TableSchema):
         qp = clean_filter(form_data, form_data['prefix'])
         if qp:
             qp = {i: v for i, v in qp[0].items() if v}
-    view = ModelView(request, params=qp, module=schema.module, model=schema.model, prefix=f'{schema.prefix}--{new_id}--')
+    view = ModelView(request, params=qp, module=schema.module, model=schema.model,
+                     prefix=f'{schema.prefix}--{new_id}--')
     return await view.get_create_line()
+
 
 class ModelSchema(BaseModel):
     module: str
@@ -188,6 +192,7 @@ class ModelSchema(BaseModel):
     def id_validate(cls, val):
         return val
 
+
 @index_router.post("/base/model_id", response_class=HTMLResponse)
 async def model_id(request: Request, schema: ModelSchema):
     """
@@ -197,6 +202,7 @@ async def model_id(request: Request, schema: ModelSchema):
     model = ModelView(request, schema.module, schema.model)
     link_view = await model.get_link_view(model_id=schema.id)
     return link_view
+
 
 class ModalSchema(BaseModel):
     prefix: str
@@ -210,6 +216,7 @@ class ModalSchema(BaseModel):
     class Config:
         extra = 'allow'
 
+
 @index_router.post("/base/modal", response_class=HTMLResponse)
 async def modal(request: Request, schema: ModalSchema):
     """
@@ -221,14 +228,15 @@ async def modal(request: Request, schema: ModalSchema):
         method_schema = getattr(model.schemas, schema.method)
         method_schema_obj = method_schema(**data[0])
         adapter_method = getattr(model.adapter, schema.method)
-        responce = await adapter_method(id=schema.id, model=schema.model, json=method_schema_obj.model_dump(mode='json'))
+        responce = await adapter_method(id=schema.id, model=schema.model,
+                                        json=method_schema_obj.model_dump(mode='json'))
         return model.send_message(f'{model.model.capitalize()}: is {schema.method.capitalize()}')
     else:
         model_method = getattr(model, f'get_{schema.method}')
         return await model_method(model_id=schema.id, target_id=schema.target_id, backdrop=schema.backdrop)
 
 
-@index_router.get("/base/dropdown-ids", response_class=HTMLResponse)
+@index_router.get("/basic/dropdown-ids", response_class=HTMLResponse)
 async def dropdown_ids(request: Request, module: str, model: str, id: str, itemlink: str, is_named=False) -> dict:
     """
      Виджет на вход получает модуль-модель-ид- и обратную ссылку если нужно, если нет будет /module/model/{id}
