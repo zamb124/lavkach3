4522f67cb036f19b6ee0eb0feb546b2525bbbff0
viktor-shved <zambas124@gmail.com>
2025-01-04 14:12:42 +0300

    Единый Энвиромент и все пропы в конструкторе асинхронные

--- backend/app/front/static/js/app.js
+++ backend/app/front/static/js/app.js
@@ -495,7 +495,7 @@ class App {
             });
 
             const elmts = elt.querySelectorAll('.modal, .uuid-create, .uuid-update, .list-uuid-create, .list-uuid-update, .uuid-get, .list-uuid-get, .babel-create, .babel-update, .list-str, .list-enum-update, .list-enum-get, .list-enum-create, .list-rel, .nest');
-            if (elt.classList.contains('modal')) {
+            if (elt.classList.contains('modal') && (elt.id !== "exampleModal")) {
                 new ModalHandler(elt);
             }
             if (elt.classList.contains('nest')) {
